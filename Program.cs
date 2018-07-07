80db03b05994f99b4dfd52837880e849d304786f
Mark Kraus <github@markekraus.com>
2018-07-07 05:42:57 -0500

    Add repro code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CaseInsensitiveOs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = string.Compare("º", "O", StringComparison.CurrentCultureIgnoreCase);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
